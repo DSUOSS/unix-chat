7046f4d6571c986d5ce886fe3011c48edfaa9738
Alex Gartner <alex@agartner.com>
2014-10-09 23:11:58 -0500

    Added socket to connect to server. Added multithreading to read and write to socket.

--- client.c
+++ client.c
@@ -4,9 +4,42 @@
 #include <ncurses.h>
 #include <signal.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <pthread.h>
+
+#define SOCKET_PATH "/tmp/chat_socket"
+
+typedef unsigned int uint;
+
 WINDOW *chat;
 WINDOW *input;
 int parent_x, parent_y;
+
+uint cSocket;
+
+uint create_socket(){
+	int s, len;
+	struct sockaddr_un remote;
+
+	s = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (s == -1){
+		perror("Failed to create socket");
+		exit(-1);
+	}
+
+	remote.sun_family = AF_UNIX;
+	strcpy(remote.sun_path, SOCKET_PATH);
+	len = strlen(remote.sun_path) + sizeof(remote.sun_family);
+
+	if (connect(s, (struct sockaddr *)&remote, len) == -1){
+		perror("Connect failed");
+		exit(-2);
+	}
+
+	return s;
+}
 
 void window_refresh_handler() {
 
@@ -25,9 +58,40 @@ void window_refresh_handler() {
 
 }
 
-int main(int argc, char *argv[]) {
-
+void recieve_manager(){
+	char *buffer;
 	char *lines[100];
+	int i, lineCount, len;
+
+	for (;;){
+		buffer = malloc(100);
+		len = read(cSocket, buffer, 100);
+		if (!len)
+			break;
+		buffer[len] = '\0';
+
+		lines[lineCount] = buffer;
+		lineCount++;
+
+		for(i=0;i<lineCount;i++) {
+			mvwprintw(chat, i, 0, "%s", lines[i]);
+		}
+		window_refresh_handler();
+	}
+}
+
+void send_manager(){
+	char line[100];
+	for(;;){
+		wgetnstr(input, line, 100);
+		write(cSocket, line, strlen(line));
+	}
+}
+
+
+int main(int argc, char *argv[]) {
+	pthread_t recieveT, sendT;
+	cSocket = create_socket();
 
 	signal(SIGWINCH, window_refresh_handler);
 
@@ -39,17 +103,11 @@ int main(int argc, char *argv[]) {
 
 	window_refresh_handler();
 
-	int i = 0;
-	for(;;i++) {
+	pthread_create(&recieveT, NULL, (void *) &recieve_manager, NULL);
+	pthread_create(&sendT, NULL, (void *) &send_manager, NULL);
 
-		window_refresh_handler();
-
-		lines[i] = malloc(100);
-		wgetnstr(input, lines[i], 100);
-
-		mvwprintw(chat, i, 0, "<%s> %s", getlogin(), lines[i]);
-
-	}
+	pthread_join(recieveT, NULL);
+	pthread_join(sendT, NULL);
 
 	return 0;
 
